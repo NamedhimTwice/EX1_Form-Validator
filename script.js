931d25e0735c4183ad4748d1dc414612bb5ea208
NamedhimTwice <66028967+NamedhimTwice@users.noreply.github.com>
2020-05-29 13:53:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+//Check Email Validity
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Not working Fam`);
+  }
+}
+
+//Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+//Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min)
+    showError(input, `This shit too short mane. Make it longer`);
+  else if (input.value.length > max) {
+    showError(input, `G'damn I said a username, not a story`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+//Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, ` ${getFieldName(input)} dont work Bruh`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//Get Field Name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event Listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+});
+
+//Check Passwords Match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value != input2.value) {
+    showError(input2, "Bro these are not the same");
+  }
+}
